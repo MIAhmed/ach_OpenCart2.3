b9c8c109ae2dfaaaf254ba888dba2af76ee069ea
jalil fulan <36225021+lujayninfoways@users.noreply.github.com>
2018-03-21 17:50:35 +0530

    Update alphacommercehub.php

--- upload/admin/language/en-gb/extension/payment/alphacommercehub.php
+++ upload/admin/language/en-gb/extension/payment/alphacommercehub.php
@@ -12,6 +12,8 @@ $_['text_sale']						 = 'Sale';
 
 // Entry
 $_['entry_email']					 = 'Merchant-id';
+$_['entry_user']					 = 'User ID';
+$_['entry_url']					 = 'Hosted Payment Page URL';
 $_['entry_test']					 = 'Sandbox Mode';
 $_['entry_transaction']				 = 'Transaction Method';
 $_['entry_debug']					 = '3dsecure';
