72e36632116ea0adea19457e0b4a0ece5fe20429
jalil fulan <36225021+lujayninfoways@users.noreply.github.com>
2018-03-28 17:07:58 +0530

    Update alphacommercehub.php

--- upload/catalog/controller/extension/payment/alphacommercehub.php
+++ upload/catalog/controller/extension/payment/alphacommercehub.php
@@ -8,14 +8,10 @@ class ControllerExtensionPaymentAlphacommercehub extends Controller {
 
 		$data['testmode'] = $this->config->get('alphacommercehub_test');
 
-		/*if (!$this->config->get('alphacommercehub_test')) {
-			$data['action'] = 'https://hubuat.alphacommercehub.com.au/pp/bf8cb1c7-33c5-451b-aa48-498c76392f95';
-		} else {
-			$data['action'] = 'https://hubuat.alphacommercehub.com.au/pp/bf8cb1c7-33c5-451b-aa48-498c76392f95';
-		}*/
+		
 $data['action'] = 'https://hubuat.alphacommercehub.com.au/pp/'.$this->config->get('alphacommercehub_url');
 		$this->load->model('checkout/order');
-
+$data['user'] = $this->config->get('alphacommercehub_user');
 		$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
 
 		if ($order_info) {
@@ -55,6 +51,15 @@ $data['action'] = 'https://hubuat.alphacommercehub.com.au/pp/'.$this->config->ge
 					'weight'   => $product['weight']
 				);
 			}
+			foreach($data['products'] as $pro){
+	$pro_name1[]=$pro['name'];
+	$pro_quantity1[]=$pro['quantity'];
+	}
+ $pro_quantity=array_sum($pro_quantity1);
+ $pro_name=str_replace(" ","",$pro_name1);
+ $data['pro_name']=$pro_name[0];
+$data['pro_quantity']=$pro_quantity;
+			
 
 			$data['discount_amount_cart'] = 0;
 
@@ -93,6 +98,7 @@ $data['Amount'] = round($amount);
 			$data['country'] = $order_info['payment_iso_code_2'];
 			$data['email'] = $order_info['email'];
 			$data['invoice'] = $this->session->data['order_id'] . ' - ' . html_entity_decode($order_info['payment_firstname'], ENT_QUOTES, 'UTF-8') . ' ' . html_entity_decode($order_info['payment_lastname'], ENT_QUOTES, 'UTF-8');
+			$data['merchanttxnid'] = $this->session->data['order_id'];
 			$data['lc'] = $this->session->data['language'];
 			$data['return'] = $this->url->link('checkout/success');
 			$data['notify_url'] = $this->url->link('extension/payment/alphacommercehub/callback', '', true);
